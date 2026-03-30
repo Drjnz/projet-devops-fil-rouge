37731a4a5a0eef7e16a6cb66436bc9e48d207738
Jeremy Nazaire <mouloumboujeremy3@gmail.com>
2026-03-30 15:51:12 +0200

    MAJ DE MON REPOS DISTANT

--- panier.js
+++ panier.js
@@ -1,6 +1,6 @@
 function calculerTotal(prix) {
     if (prix > 100) {
-        return prix * 0.8; // Appliquer une réduction de 20%
+        return prix * 0.8; // Applique une réduction de 20% pour les prix supérieurs à 100
     }
     return prix;
 }
